332119b526e25dc7adb520fe6fc0560914e7f5d2
Dave Xiao <xdwdave@gmail.com>
2016-02-12 13:14:42 +0800

    Add ForecastDisplay

--- WeatherStation2/README.md
+++ WeatherStation2/README.md
@@ -3,6 +3,7 @@ javac Observer.java
 javac Subject.java
 javac WeatherData.java
 javac CurrentConditionsDisplay.java
+javac ForecastDisplay.java
 javac WeatherStation.java
 
 java WeatherStation
--- WeatherStation2/WeatherStation.java
+++ WeatherStation2/WeatherStation.java
@@ -6,6 +6,8 @@ public class WeatherStation {
 
         CurrentConditionsDisplay currentDisplay =
             new CurrentConditionsDisplay(weatherData);
+        ForecastDisplay forecastDisplay =
+            new ForecastDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
